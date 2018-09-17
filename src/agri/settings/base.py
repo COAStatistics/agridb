3fd974dd1014efebc07265e81334491bc44e6819
unknown <1042080@atri.org.tw>
2018-09-17 15:42:52 +0800

    Prepare deploy settings
    New requirements
      - upgrade Django 2.0.6 -> 2.0.8
      - install wfastcgi for deploy iis server
      - install pyodbc for connect SQL Server 2014
      - install whitenoise for server static files
    Add static files url when debug True
    Add static with whitenoise settings in base.py

--- src/agri/settings/base.py
+++ src/agri/settings/base.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -116,6 +117,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 FIXTURE_DIRS = [
