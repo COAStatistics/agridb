92dfa512e70fc248b9c6d752a91d38719fff2fc7
MatsuiLin101 <matsuilin101@gmail.com>
2018-10-01 09:17:32 +0800

    Fix APIView permission
    Change permission_class to permission_classes.
    Add auth to base.

--- src/fallow/api/views.py
+++ src/fallow/api/views.py
@@ -13,58 +13,58 @@ from . import serializers
 class CropListCreateAPIView(generics.ListCreateAPIView):
     serializer_class = serializers.CropSerializers
     queryset = Crop.objects.all()
-    permission_class = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
 
 
 class DeclareListCreateAPIView(generics.ListCreateAPIView):
     serializer_class = serializers.DeclareSerializers
     queryset = Declare.objects.all()
-    permission_class = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
 
 
 class RiceAreaListCreateAPIView(generics.ListCreateAPIView):
     serializer_class = serializers.RiceAreaSerializers
     queryset = RiceArea.objects.all()
-    permission_class = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
 
 
 class FallowTransferListCreateAPIView(generics.ListCreateAPIView):
     serializer_class = serializers.FallowTransferSerializers
     queryset = FallowTransfer.objects.all()
-    permission_class = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
 
 
 class TransferCropListCreateAPIView(generics.ListCreateAPIView):
     serializer_class = serializers.TransferCropSerializers
     queryset = TransferCrop.objects.all()
-    permission_class = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
 
 
 class CropRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = serializers.CropSerializers
     queryset = Crop.objects.all()
-    permission_class = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
 
 
 class DeclareRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = serializers.DeclareSerializers
     queryset = Declare.objects.all()
-    permission_class = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
 
 
 class RiceAreaRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = serializers.RiceAreaSerializers
     queryset = RiceArea.objects.all()
-    permission_class = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
 
 
 class FallowTransferRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = serializers.FallowTransferSerializers
     queryset = FallowTransfer.objects.all()
-    permission_class = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
 
 
 class TransferCropRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = serializers.TransferCropSerializers
     queryset = TransferCrop.objects.all()
-    permission_class = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
