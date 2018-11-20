dc0a9b18db5d2722b740c5d2f07dcd9cd36b13f1
MatsuiLin101 <matsuilin101@gmail.com>
2018-11-20 16:56:11 +0800

    Modified disaster and smallbig model
    Disaster app disaster model change subsidy field datatype to int.
    SmallBig model add year field to all table.
    Create smallbig all hash api.

--- src/smallbig/api/views.py
+++ src/smallbig/api/views.py
@@ -33,6 +33,106 @@ class TenantListCreateAPIView(generics.ListCreateAPIView):
     pagination_class = ThousandPagination
 
 
+class LandlordRetireHashListAPIView(generics.ListCreateAPIView):
+    serializer_class = serializers.LandlordRetireSerializer
+    queryset = models.LandlordRetire.objects.all()
+    permission_classes = [IsAuthenticated]
+    pagination_class = ThousandPagination
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            data = {}
+            for i in serializer.data:
+                data[i['member']] = i['id']
+            return self.get_paginated_response(data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        data = {}
+        for i in serializer.data:
+            data[i['member']] = i['id']
+
+        return Response(data)
+
+
+class LandlordRentHashListAPIView(generics.ListCreateAPIView):
+    serializer_class = serializers.LandlordRentSerializer
+    queryset = models.LandlordRent.objects.all()
+    permission_classes = [IsAuthenticated]
+    pagination_class = ThousandPagination
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            data = {}
+            for i in serializer.data:
+                data[i['member']] = i['id']
+            return self.get_paginated_response(data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        data = {}
+        for i in serializer.data:
+            data[i['member']] = i['id']
+
+        return Response(data)
+
+
+class TenantTransferHashListAPIView(generics.ListCreateAPIView):
+    serializer_class = serializers.TenantTransferSerializer
+    queryset = models.TenantTransfer.objects.all()
+    permission_classes = [IsAuthenticated]
+    pagination_class = ThousandPagination
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            data = {}
+            for i in serializer.data:
+                data[i['member']] = i['id']
+            return self.get_paginated_response(data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        data = {}
+        for i in serializer.data:
+            data[i['member']] = i['id']
+
+        return Response(data)
+
+
+class TenantHashListAPIView(generics.ListCreateAPIView):
+    serializer_class = serializers.TenantSerializer
+    queryset = models.Tenant.objects.all()
+    permission_classes = [IsAuthenticated]
+    pagination_class = ThousandPagination
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            data = {}
+            for i in serializer.data:
+                data[i['member']] = i['id']
+            return self.get_paginated_response(data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        data = {}
+        for i in serializer.data:
+            data[i['member']] = i['id']
+
+        return Response(data)
+
+
 class LandlordRetireRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = serializers.LandlordRetireSerializer
     queryset = models.LandlordRetire.objects.all()
