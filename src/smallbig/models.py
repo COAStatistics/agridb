51da54ccf13ab351dcd9ba1510550f28f48b5c9d
MatsuiLin101 <matsuilin101@gmail.com>
2018-11-20 16:56:11 +0800

    Modified disaster and smallbig model
    Disaster app disaster model change subsidy field datatype to int.
    SmallBig model add year field to all table.
    Create smallbig all hash api.

--- src/smallbig/models.py
+++ src/smallbig/models.py
@@ -3,35 +3,39 @@ from django.db import models
 
 class LandlordRetire(models.Model):
     member = models.ForeignKey('household.Member', on_delete=models.CASCADE, verbose_name='Member')
+    year = models.ForeignKey('household.Year', on_delete=models.CASCADE, verbose_name='Year', default=1)
     subsidy = models.IntegerField(null=True, verbose_name='Subsidy')
     update_time = models.DateTimeField(auto_now=True, auto_now_add=False, null=True, blank=True, verbose_name='Updated')
 
     def __str__(self):
-        return self.member
+        return "{}  {}  {}".format(self.year, self.member, self.subsidy)
 
 
 class LandlordRent(models.Model):
     member = models.ForeignKey('household.Member', on_delete=models.CASCADE, verbose_name='Member')
+    year = models.ForeignKey('household.Year', on_delete=models.CASCADE, verbose_name='Year', default=1)
     subsidy = models.IntegerField(null=True, verbose_name='Subsidy')
     update_time = models.DateTimeField(auto_now=True, auto_now_add=False, null=True, blank=True, verbose_name='Updated')
 
     def __str__(self):
-        return self.member
+        return "{}  {}  {}".format(self.year, self.member, self.subsidy)
 
 
 class TenantTransfer(models.Model):
     member = models.ForeignKey('household.Member', on_delete=models.CASCADE, verbose_name='Member')
+    year = models.ForeignKey('household.Year', on_delete=models.CASCADE, verbose_name='Year', default=1)
     subsidy = models.IntegerField(null=True, verbose_name='Subsidy')
     update_time = models.DateTimeField(auto_now=True, auto_now_add=False, null=True, blank=True, verbose_name='Updated')
 
     def __str__(self):
-        return self.member
+        return "{}  {}  {}".format(self.year, self.member, self.subsidy)
 
 
 class Tenant(models.Model):
     member = models.ForeignKey('household.Member', on_delete=models.CASCADE, related_name='tenant_member_set', verbose_name='Member')
     owner = models.ForeignKey('household.Member', on_delete=models.CASCADE, related_name='tenant_owner_set', verbose_name='Landlord')
+    year = models.ForeignKey('household.Year', on_delete=models.CASCADE, verbose_name='Year', default=1)
     update_time = models.DateTimeField(auto_now=True, auto_now_add=False, null=True, blank=True, verbose_name='Updated')
 
     def __str__(self):
-        return self.member
+        return "{}  {}  {}".format(self.year, self.member, self.owner)
