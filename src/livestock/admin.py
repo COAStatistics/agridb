2909af3d9e1cd07297e5eeb1ecff43efdc2e892d
travishen.tw <travishen.tw@gmail.com>
2018-11-20 16:27:56 +0800

    Update livestock

--- src/livestock/admin.py
+++ src/livestock/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
-from .models import Profile
+from .models import (
+    Profile,
+    Livestock,
+    InvestigationType,
+    Investigation,
+    Field,
+    CountType,
+)
 
 # Register your models here.
+admin.site.register(Livestock)
+admin.site.register(InvestigationType)
+admin.site.register(Investigation)
+admin.site.register(Field)
+admin.site.register(CountType)
 admin.site.register(Profile)
